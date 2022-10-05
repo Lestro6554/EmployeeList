e1044f4a2d5a3337f813e4eb3c138f5eebd65f6f
Lestro6554 <lex18908@mail.ru>
2022-10-05 11:15:42 +0400

    build

--- src/models/IEmployee.ts
+++
@@ -1,9 +0,0 @@
-export default interface IEmployee {
-    id: number,
-    fio: string,
-    position: string,
-    birthdate?: Date | null,
-    gender?: string | null,
-    isDismissed?: boolean,
-    isActive?: boolean 
-};
